fcd4b4e1e5aa05dabf87cc506a36284ce8d2e2ae
S. Ross <56163785+gy19sr@users.noreply.github.com>
2020-09-01 16:38:41 +0200

    Update Solved.py

--- Solved.py
+++ Solved.py
@@ -5,6 +5,45 @@ Created on Tue Sep  1 11:10:25 2020
 @author: stuart
 """
 
+
+# 1431. Kids With the Greatest Number of Candies
+#
+"""
+class Solution:
+    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+        num_of_kids = candies
+        i = 0 # the number of kids 
+        while i < len(num_of_kids):
+            print("test")
+            i += 1
+  """          
+from typing import List
+
+class Solution:
+    def kidsWithCandies(self, candies: List[int]) -> int:
+        num_of_kids = candies
+        i = 0 # the number of kids 
+        while i < len(num_of_kids):
+            print("test")
+            i += 1
+        if i == len(num_of_kids):
+            return 
+            
+ans = Solution()
+print(ans.kidsWithCandies([14, 11, 22]))
+            
+
+
+
+
+
+
+
+
+
+
+
+
 """
 1342. Number of Steps to Reduce a Number to Zero
 
